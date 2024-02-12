5391ebbddfd2f8bf7b74e7b4ec6c1f7fdada15c6
a2-imeri <Alfret2.imeri@live.uwe.ac.uk>
2024-02-12 12:30:30 +0000

    Basic Implementation of Task 1

--- flash_ops.c
+++ flash_ops.c
@@ -18,6 +18,25 @@
 //
 // Note: This function erases the flash sector before writing new data.
 void flash_write_safe(uint32_t offset, const uint8_t *data, size_t data_len) {
+ 
+
+ 
+
+
+uint32_t flash_offset = FLASH_TARGET_OFFSET + offset;
+
+
+uint32_t ints = save_and_disable_interrupts();
+
+flash_range_erase(flash_offset, FLASH_SECTOR_SIZE);
+
+
+flash_range_program(flash_offset, data, data_len);
+
+restore_interrupts(ints);
+
+
+
 }
 
 // Function: flash_read_safe
@@ -30,15 +49,40 @@ void flash_write_safe(uint32_t offset, const uint8_t *data, size_t data_len) {
 //
 // Note: The function performs bounds checking to ensure safe access.
 void flash_read_safe(uint32_t offset, uint8_t *buffer, size_t buffer_len) {
+
+uint32_t flash_offset = FLASH_TARGET_OFFSET + offset;
+
+memcpy(buffer, (const void *)(XIP_BASE + flash_offset), buffer_len);
+
+} 
+void flash_erase_safe(uint32_t offset) {
+
+	uint32_t flash_offset = FLASH_TARGET_OFFSET + offset;
+	uint32_t ints = save_and_disable_interrupts();
+	uint32_t sector_start = flash_offset & ~(FLASH_SECTOR_SIZE - 1);
+	if (sector_start < FLASH_TARGET_OFFSET + FLASH_SIZE) {
+		flash_range_erase(sector_start, FLASH_SECTOR_SIZE);
+	} else {
+        	printf("Error: Sector start address is out of bounds.\n");
+        	// Restore interrupts before returning due to error
+        	restore_interrupts(ints);
+        	return;
+    	}
+
+    // Restore Interrupts
+    restore_interrupts(ints);
 }
 
-// Function: flash_erase_safe
-// Erases a sector of the flash memory.
-//
-// Parameters:
-// - offset: The offset from FLASH_TARGET_OFFSET where the erase starts.
-//
-// Note: This function checks that the operation stays within valid bounds.
-void flash_erase_safe(uint32_t offset) {
-}
+
+
+
+
+
+
+
+
+
+
+
+
 
